607a01cfd7dc1a8ddb8d5efcd27abf07256b5526
Mohammad Azam <mohdazam0311@gmail.com>
2026-02-18 20:40:57 +0530

    Auth controller updated

--- src/main/java/com/example/quizapp/inventorymanagement/controller/AuthController.java
+++ src/main/java/com/example/quizapp/inventorymanagement/controller/AuthController.java
@@ -12,7 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "http://localhost:5174")
 @RequestMapping("/api/auth")
 public class AuthController {
 
